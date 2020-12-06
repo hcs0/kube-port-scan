8d0198b428c2fba7332cb35f7e54833ef2eaa4a0
Hannah Suarez/hcs0 <azure@suarez.id.au>
2020-12-06 23:43:19 +0100

    Fix typo

--- scan.py
+++ scan.py
@@ -22,7 +22,7 @@ if resp == '1':
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, '1-1024', '-v -sS')
     print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
+    print("IP Status: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     #tcp key error on line 27
     print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
@@ -30,14 +30,14 @@ elif resp == '2':
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, '1-1024', '-v -sU')
     print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
+    print("IP Status: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("Open Ports: ", scanner[ip_addr]['udp'].keys())
 elif resp == '3':
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
     print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
+    print("IP Status: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
     print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
 elif resp >= '4':
